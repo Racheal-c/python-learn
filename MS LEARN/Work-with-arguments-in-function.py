9a97fb852967ba783e05c49caca6aee30f2c963f
Racheal Chek <74939355+Racheal-c@users.noreply.github.com>
2023-05-02 06:58:26 +0800

    Use functions in Python

---
+++ MS LEARN/Work-with-arguments-in-function.py
@@ -0,0 +1,10 @@
+# Exercise: Work with arguments in functions
+def generate_report(main_tank, external_tank, hydrogen_tank):
+    output = f"""Fuel Report:
+    Main tank: {main_tank}
+    External tank: {external_tank}
+    Hydrogen tank: {hydrogen_tank} 
+    """
+    print(output)
+
+generate_report(60,70,80)
