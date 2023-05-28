bdc647bcf64b1cf34c47875ed4020e244ef75d16
Racheal Chek <74939355+Racheal-c@users.noreply.github.com>
2023-05-29 07:54:10 +0800

    dictionary vs list

---
+++ py4e/dictionary-vs-list.py
@@ -0,0 +1,20 @@
+#lists (use numbers to look up VALUES, only accept numbers)
+lst = list()
+lst.append(21)
+lst.append(183)
+print(lst)
+# this is the code that shows using [0], [1] (aka numbers) to get the value
+lst[0] = 23
+print(lst)
+
+#dictionary (use keys to look up VALUES)
+ddd = dict()
+ddd['age'] = 21
+ddd['cOurse'] = 180
+print(ddd)
+ddd['age'] = 25
+print(ddd)
+
+#make empty dictionary using empty curly braces
+ooo = {}
+print(ooo)
