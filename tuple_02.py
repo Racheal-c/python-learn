acd9239842784422495b577382ae17daddb74712
Racheal Chek <74939355+Racheal-c@users.noreply.github.com>
2023-05-28 09:04:01 +0800

    Comparing and sorting tuples

--- tuple_02.py
+++ tuple_02.py
@@ -1,6 +1,19 @@
-lst = []
-for key, val in counts.items():
-    newtup = (val, key)
-    lst.append(newtup)
-lst = sorted(lst, reverse=True)
-print(lst)
+d = {'a':10, 'b':1, 'c':20}
+d.items()
+sorted(d.items())
+#use sorted
+d = {'a':10, 'b':1, 'c':20}
+t = sorted(d.items())
+t
+
+for k,v in sorted(d.items()):
+    print(k,v)
+
+#sort by values instead of key
+a = {'a':10, 'b':1, 'c':20}
+tmp = list()
+for k, v in a.items() :
+    tmp.append( (v,k) )
+print (tmp)
+tmp = sorted(tmp, reverse=True)
+print(tmp) 
